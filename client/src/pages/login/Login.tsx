60aa7cebf6612bec67cb3e9eb8e5284f63c47d76
Elem <elem15@yandex.ru>
2023-05-05 22:43:42 +0300

    feat: add register & login

--- client/src/pages/login/Login.tsx
+++ client/src/pages/login/Login.tsx
@@ -1,9 +1,30 @@
+import { Card, Form, Row, Space, Typography } from 'antd';
 import { Layout } from '../../components/layout/Layout';
+import { CustomInput } from '../../components/CustomInput/CustomInput';
+import { PasswordInput } from '../../components/PasswordInput/PasswordInput';
+import { CustomButton } from '../../components/CustomButton/CustomButton';
+import { Link } from 'react-router-dom';
+import { Paths } from '../../paths';
 
 export const Login = () => {
   return (
     <Layout>
-      <div>Login</div>
+      <Row align="middle" justify="center">
+        <Card title="SignIn" style={{ width: '30rem' }}>
+          <Form onFinish={() => null}>
+            <CustomInput name="email" placeholder="email" />
+            <PasswordInput name="password" placeholder="password" />
+            <CustomButton type="primary" htmlType="submit">
+              Login
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              No account? <Link to={Paths.register}>Register</Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   );
 };
--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -1,9 +1,32 @@
+import { Card, Form, Row, Space, Typography } from 'antd';
 import { Layout } from '../../components/layout/Layout';
+import { CustomInput } from '../../components/CustomInput/CustomInput';
+import { PasswordInput } from '../../components/PasswordInput/PasswordInput';
+import { CustomButton } from '../../components/CustomButton/CustomButton';
+import { Link } from 'react-router-dom';
+import { Paths } from '../../paths';
 
 export const Register = () => {
   return (
     <Layout>
-      <div>Register</div>
+      <Row align="middle" justify="center">
+        <Card title="Register" style={{ width: '30rem' }}>
+          <Form onFinish={() => null}>
+            <CustomInput name="name" placeholder="name" />
+            <CustomInput name="email" placeholder="email" />
+            <PasswordInput name="password" placeholder="password" />
+            <PasswordInput name="confirm" placeholder="confirm password" />
+            <CustomButton type="primary" htmlType="submit">
+              Register
+            </CustomButton>
+          </Form>
+          <Space direction="vertical" size="large">
+            <Typography.Text>
+              Already registered? <Link to={Paths.login}>Login </Link>
+            </Typography.Text>
+          </Space>
+        </Card>
+      </Row>
     </Layout>
   );
 };
