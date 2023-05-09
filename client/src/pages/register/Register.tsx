d7896a8f2f715f141f373051e007265c2d2a255e
Elem <elem15@yandex.ru>
2023-05-09 22:14:10 +0300

    feat: add user registration

--- client/src/pages/register/Register.tsx
+++ client/src/pages/register/Register.tsx
@@ -3,15 +3,38 @@ import { Layout } from '../../components/layout/Layout';
 import { CustomInput } from '../../components/CustomInput/CustomInput';
 import { PasswordInput } from '../../components/PasswordInput/PasswordInput';
 import { CustomButton } from '../../components/CustomButton/CustomButton';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { Paths } from '../../paths';
+import { UserData, useRegisterMutation } from '../../app/services/auth';
+import { useState } from 'react';
+import { isErrorWithMessage } from '../../utils/isErrorWithMessage';
+import { ErrorMessage } from '../../components/ErrorMessage/ErrorMessage';
 
 export const Register = () => {
+  const navigate = useNavigate();
+  const [registerUser, registerUserResult] = useRegisterMutation();
+  const [error, setError] = useState('');
+  const register = async (data: UserData) => {
+    try {
+      await registerUser(data).unwrap();
+      navigate(Paths.home);
+    } catch (e) {
+      const maybeError = isErrorWithMessage(e);
+      if (maybeError) {
+        setError(e.data.message);
+      } else {
+        setError('Unknown error');
+      }
+    }
+  };
   return (
     <Layout>
       <Row align="middle" justify="center">
         <Card title="Register" style={{ width: '30rem' }}>
-          <Form onFinish={() => null}>
+          <Form
+            // eslint-disable-next-line @typescript-eslint/no-misused-promises
+            onFinish={register}
+          >
             <CustomInput name="name" placeholder="name" />
             <CustomInput name="email" placeholder="email" />
             <PasswordInput name="password" placeholder="password" />
@@ -26,6 +49,7 @@ export const Register = () => {
             </Typography.Text>
           </Space>
         </Card>
+        <ErrorMessage message={error} />
       </Row>
     </Layout>
   );
